667cb1c7e2f36e083e48220c8bcf91ab13182837
Uzziel <20243ds122@utez.edu.mx>
2025-12-10 02:04:33 -0600

    añadi la clase pedido atributos y un constructor todavia vacio

--- src/main/java/com/example/FastFood_Service/model/Pedido.java
+++ src/main/java/com/example/FastFood_Service/model/Pedido.java
@@ -1,4 +1,11 @@
 package com.example.FastFood_Service.model;
 
 public class Pedido {
-}
+
+    private int id;
+    private String nombreCliente;
+    private String descripcion;
+    private double monto;
+    private String estado;
+
+    public Pedido() {}
