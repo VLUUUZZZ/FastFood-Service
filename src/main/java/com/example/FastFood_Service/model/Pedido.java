42eff7a3798e6ee40f5a578c807b77cd5403e3d2
Uzziel <20243ds122@utez.edu.mx>
2025-12-10 02:05:46 -0600

    complete la clase pedido con su contructor completo y sus getters y setters

--- src/main/java/com/example/FastFood_Service/model/Pedido.java
+++ src/main/java/com/example/FastFood_Service/model/Pedido.java
@@ -8,4 +8,26 @@ public class Pedido {
     private double monto;
     private String estado;
 
-    public Pedido() {}
+    public Pedido(int id, String nombreCliente, String descripcion, double monto, String estado) {
+        this.id = id;
+        this.nombreCliente = nombreCliente;
+        this.descripcion = descripcion;
+        this.monto = monto;
+        this.estado = estado;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombreCliente() { return nombreCliente; }
+    public void setNombreCliente(String nombreCliente) { this.nombreCliente = nombreCliente; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public double getMonto() { return monto; }
+    public void setMonto(double monto) { this.monto = monto; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+}
