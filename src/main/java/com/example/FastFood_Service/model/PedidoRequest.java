c10058db4f13a7f450f752d8500541b30ea2e0c9
Uzziel <20243ds122@utez.edu.mx>
2025-12-10 02:08:10 -0600

    se completo PedidoRequest con sus contructores completos, getters y setters

--- src/main/java/com/example/FastFood_Service/model/PedidoRequest.java
+++ src/main/java/com/example/FastFood_Service/model/PedidoRequest.java
@@ -6,6 +6,19 @@ public class PedidoRequest {
     private String descripcion;
     private double monto;
 
-    public PedidoRequest() {
-
+    public PedidoRequest(String nombreCliente, String descripcion, double monto) {
+        this.nombreCliente = nombreCliente;
+        this.descripcion = descripcion;
+        this.monto = monto;
     }
+
+    public String getNombreCliente() { return nombreCliente; }
+    public void setNombreCliente(String nombreCliente) { this.nombreCliente = nombreCliente; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public double getMonto() { return monto; }
+    public void setMonto(double monto) { this.monto = monto; }
+}
+
